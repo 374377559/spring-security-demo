75d0c7a3ccbf4b3627d1a1e58527ca1a8812a724
fnyexx <fnyexx@mayduo>
2013-01-09 17:14:44 +0800

    权限管理

---
+++ src/com/huaxin/dao/NoticeDao.java
@@ -0,0 +1,10 @@
+package com.huaxin.dao;
+
+import java.util.List;
+
+import com.huaxin.bean.Notice;
+import com.huaxin.dao.base.BaseDao;
+
+public interface NoticeDao extends BaseDao<Notice> {
+	List<Notice> findAll();
+}
---
+++ src/com/huaxin/dao/impl/NoticeDaoImpl.java
@@ -0,0 +1,9 @@
+package com.huaxin.dao.impl;
+
+import com.huaxin.bean.Notice;
+import com.huaxin.dao.NoticeDao;
+import com.huaxin.dao.base.impl.HibernateBaseDao;
+
+public class NoticeDaoImpl extends HibernateBaseDao<Notice> implements NoticeDao{
+
+}
